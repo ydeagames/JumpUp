b677c93adb10abaafcfe3ca3348655c5fb63b7b0
YdeaGames <ydeagames@gmail.com>
2020-01-16 18:20:50 +0900

    Fade

---
+++ Assets/Objects/Fade/MyFade.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyFade : MonoBehaviour
+{
+    Fade fade;
+
+    static MyFade instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        fade = GetComponent<Fade>();
+    }
+
+    public void Fadeout(string scene)
+    {
+        StartCoroutine(FadeoutCoroutine(scene));
+    }
+
+    IEnumerator FadeoutCoroutine(string scene)
+    {
+        for (float time = 0; time < 1; time += Time.deltaTime)
+        {
+            fade.fade.Range = Mathf.Clamp(time, 0, 1);
+            yield return null;
+        }
+
+        fade.fade.Range = 1;
+        SceneManager.LoadScene(scene);
+
+        for (float time = 0; time < 1; time += Time.deltaTime)
+        {
+            fade.fade.Range = Mathf.Clamp(1 - time, 0, 1);
+            yield return null;
+        }
+
+        fade.fade.Range = 0;
+        yield break;
+    }
+
+    public static MyFade Get()
+    {
+        return instance;
+    }
+}
--- Assets/Objects/Shared/SceneTransition.cs
+++ Assets/Objects/Shared/SceneTransition.cs
@@ -1,17 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+    private Fade fade;
+
+    private void Start()
+    {
+    }
+
     public void LoadScene(string scene)
     {
-        SceneManager.LoadScene(scene);
+        MyFade.Get().Fadeout(scene);
     }
 
     public void Reload()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        LoadScene(SceneManager.GetActiveScene().name);
     }
 }
