342fe936de7aea5e2dc0a07a7c96ffbec74ed02f
GULLD20 <itikiyuji@i.softbank.jp>
2020-01-17 12:32:27 +0900

    タイマー・スコア(仮)・ライティング(マグマなので下から赤い光が出ている)

---
+++ Assets/Objects/Canvas/ScoreController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    //距離を計算するターゲットオブジェクト
+    [SerializeField]
+    private Transform targetObj1;
+    //距離を計算するターゲットオブジェクト
+    [SerializeField]
+    private Transform targetObj2;
+    //スコア用のテキスト
+    private Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       scoreText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //　距離を計算
+        var distance = Vector3.Distance(transform.position, targetObj2.position);
+
+        if (scoreText != null)
+        {
+            scoreText.text = distance.ToString("0.00m");
+        }
+        else
+        {
+ 
+        }
+    }
+}
---
+++ Assets/Objects/Canvas/TimerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField]
+    private int minute;     //分
+    [SerializeField]
+    private float seconds;  //秒
+    //前のUpdateの時の秒数
+    private float oldSeconds;
+    //タイマー用テキスト
+    private Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minute = 0;
+        seconds = 0f;
+        oldSeconds = 0f;
+        timerText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds += Time.deltaTime;
+        if (seconds >= 60f)
+        {
+            minute++;
+            seconds = seconds - 60;
+        }
+        //　値が変わった時だけテキストUIを更新
+        if ((int)seconds != (int)oldSeconds)
+        {
+            timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+        }
+        oldSeconds = seconds;
+    }
+}
