971b9b533c470e4ae4c37206ae981ba9b69b1e4d
GULLD20 <itikiyuji@i.softbank.jp>
2020-01-20 16:16:41 +0900

    すり抜け床・落ちる床(仮)

---
+++ Assets/Objects/Tiles/FallingFloorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingFloorController : MonoBehaviour
+{
+    bool checkFalling = false;
+    float FallingSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FallingSpeed = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (checkFalling)
+        {
+            FallingSpeed += 0.001f;
+            transform.position = new Vector3(transform.position.x, transform.position.y - FallingSpeed, transform.position.z);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        checkFalling = true;
+        Debug.Log(checkFalling);
+    }
+}
---
+++ Assets/T90/T90Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T90Controller : MonoBehaviour
+{
+    float posX = 0;
+    bool moveCheck;
+    GameManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameManager.Get();
+        moveCheck = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (manager.player.transform.position.y < transform.position.y && manager.player.transform.position.y > transform.position.y - 10)
+        {
+            moveCheck = true;
+        }
+
+        if (moveCheck)
+        {
+            posX += 0.001f;
+            transform.position = new Vector3(transform.position.x - posX, transform.position.y, transform.position.z);
+        }
+    }
+}
